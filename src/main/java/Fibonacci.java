c140bfb4499f2fbad28d35e8fa478f8b5a199229
tomilyk <tomilyk@gmail.com>
2024-10-18 15:50:52 -0600

    Se agrega seucesion de Fibonacci

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce la cantidad de términos de la sucesión de Fibonacci: ");
+        int n = scanner.nextInt();
+
+        int t1 = 0, t2 = 1;
+        System.out.print("Primeros " + n + " términos: ");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(t1 + " ");
+
+            // Calcula el siguiente término
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
